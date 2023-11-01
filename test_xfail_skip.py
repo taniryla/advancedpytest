1b4b8d0e55a88168b69fb16e0790bde8da618b27
Chako <ryland0000@gmail.com>
2023-11-01 10:18:00 -0700

    pytest-xfail-skip 2

--- test_xfail_skip.py
+++ test_xfail_skip.py
@@ -8,6 +8,15 @@ def func_retval():
 def test_func_one(input_value):
     assert func_retval() < input_value
 
+@pytest.mark.markone
 @pytest.mark.skip
 def test_func_two(input_value):
+    assert func_retval() > input_value
+
+@pytest.mark.markone
+def test_func_three(input_value):
+    assert func_retval() == input_value
+
+@pytest.mark.marktwo
+def test_func_four(input_value):
     assert func_retval() > input_value
